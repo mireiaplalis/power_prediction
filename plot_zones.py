f7cb55f61a765f03608efe9e3b94d98d844f9589
JochemTSR <jochem@jochemram.net>
2022-11-23 23:52:43 +0100

    Added code to create grayscale images of inflows

--- plot_zones.py
+++ plot_zones.py
@@ -1,10 +1,16 @@
 from entsoe.geo.utils import load_zones
 import plotly.express as px
 import pandas as pd
+import entsoe
 
-zones = ['NL', 'BE', 'DE_LU']
+avail_zones = [
+    "AT", "BE", "BG", "CH", "CZ", "DE_LU", "DK_1", "DK_2", "EE",
+    "ES", "FI", "FR", "GR", "HR", "HU", "IT_CNOR", 
+    "IT_CSUD", "IT_NORD", "IT_SARD", "IT_SICI", "IT_SUD", "LT", 
+    "LV", "NL", "NO_1", "NO_2", "NO_3", "NO_4", "NO_5", "PL", "PT", 
+    "RO", "RS", "SE_1", "SE_2", "SE_3", "SE_4", "SI", "SK"]
 
-geo_df = load_zones(zones, pd.Timestamp(202201010000))
+geo_df = load_zones(avail_zones, pd.Timestamp(202201010000))
 geo_df['value'] = range(1,len(geo_df)+1)
 
 fig = px.choropleth(geo_df,
