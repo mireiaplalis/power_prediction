f0ad0bc81b1ef4760d43198de66e77eaae3c14af
Mireia <mireiaplalis@github.com>
2023-01-05 18:21:38 +0100

    Data preprocessing

--- inflow_plotter.py
+++ inflow_plotter.py
@@ -8,6 +8,8 @@ import plotly.express as px
 from entsoe.geo.utils import load_zones
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+
 
 TIMEZONE="Europe/Brussels"
 
@@ -23,6 +25,7 @@ class InflowPlotter:
 			self.inflow_data = pd.read_csv(inflow_data, parse_dates=True, index_col=0)
 		except:
 			raise RuntimeError("Failed to read data from file {}".format(inflow_data))
+		self.data_preprocessing()
 	
 
 	def plot_timestamp(self, timestamp: pd.Timestamp) -> plotly.graph_objs.Figure:
@@ -44,7 +47,7 @@ class InflowPlotter:
 		inflows = pd.Series(inflows)
 		
 		# Plot (save and show as desired)
-		geo_df = load_zones(self.zone_list, rounded_timestamp.tz_localize(None))
+		geo_df = load_zones(self.zone_list, rounded_timestamp.tz_localize(None), allow_cala=True)
 		inflows.name = "inflows"
 		df = geo_df.join(inflows)
 		fig = px.choropleth(df,
@@ -54,8 +57,39 @@ class InflowPlotter:
 			projection="mercator",
 			color_continuous_scale='greys')
 		fig.update_geos(fitbounds="locations", visible=False)
-		
+		fig.show()
 		return fig
+
+	def data_preprocessing(self):
+		self.inflow_data.fillna(0)
+		print("Data preprocessing")
+		bar = tqdm(total=len(self.inflow_data.index))
+		for index, _ in self.inflow_data.iterrows():
+			index_str = str(index)
+			timestamp = pd.Timestamp(index_str)
+			rounded_timestamp = timestamp.floor(freq='H')
+			if timestamp.tz_localize(None) < pd.Timestamp('2021-01-01'):
+				print("hello")
+				time_entry = self.inflow_data.loc[rounded_timestamp]
+				for name, _ in time_entry.items():
+					print("hey")
+					country_from, country_to = str(name).split('>')
+					print("hey 2")
+					if country_from == "IT_SUD":
+						self.inflow_data["IT_SUD"+">"+country_to] /= 2
+						self.inflow_data["IT_CALA"+">"+country_to] = self.inflow_data["IT_SUD"+">"+country_to]
+					print("hey 3")
+					if country_to == "IT_SUD":
+						it = 0
+						print("hey 4")
+						self.inflow_data[country_from+">"+"IT_SUD"] /= 2
+						print("hey 5")
+						self.inflow_data[country_from+">"+"IT_CALA"] = self.inflow_data[country_from+">"+"IT_SUD"]	
+					print("hey 6")
+					print(name)
+				print(timestamp)
+			bar.update()					
+
 	
 	def create_flow_matrix(self, timestamp: pd.Timestamp) -> np.ndarray:
 		# Round timestamp to nearest hour
@@ -88,28 +122,59 @@ class InflowPlotter:
 		bar = tqdm(total=len(self.inflow_data.index))
 		figure_list = []
 		timestamp_list = []
-		for index, _ in self.inflow_data.iterrows():
-			index_str = str(index)
-			timestamp = pd.Timestamp(index_str)
-			unix_timestamp = int(timestamp.timestamp())
-			timestamp_list.append(unix_timestamp)
+		
+		##### DEBUG #####
+		for i, (index, _) in enumerate(self.inflow_data.iterrows()):
+			if i % (365 * 24) == 0:
+				index_str = str(index)
+				timestamp = pd.Timestamp(index_str)
+				unix_timestamp = int(timestamp.timestamp())
+				timestamp_list.append(unix_timestamp)
 
-			figure = None
+				figure = None
 
-			if format == "matrices":
-				figure = self.create_flow_matrix(pd.Timestamp(index_str))
-				figure_list.append(figure)
+				if format == "matrices":
+					figure = self.create_flow_matrix(pd.Timestamp(index_str))
+					figure_list.append(figure)
 
-			elif format == "images":
-				figure = self.plot_timestamp(pd.Timestamp(index_str))
-				figure.update_coloraxes(showscale=False)
-				figure.update_layout(width=res, height=res)
-				buf = io.BytesIO(figure.to_image(format="png"))
-				img = Image.open(buf)
-				figure_array = np.asarray(img)
-				figure_list.append(figure)
+				elif format == "images":
+					figure = self.plot_timestamp(pd.Timestamp(index_str))
+					figure.update_coloraxes(showscale=False)
+					figure.update_layout(width=res, height=res)
+					plt.savefig(figure_output + index_str + ".png")
+					buf = io.BytesIO(figure.to_image(format="png"))
+					img = Image.open(buf)
+					figure_array = np.asarray(img)
+					figure_list.append(figure)
 
-			bar.update()
+
+				bar.update()
+
+
+
+		##################
+		# for index, _ in self.inflow_data.iterrows():
+		# 	index_str = str(index)
+		# 	timestamp = pd.Timestamp(index_str)
+		# 	unix_timestamp = int(timestamp.timestamp())
+		# 	timestamp_list.append(unix_timestamp)
+
+		# 	figure = None
+
+		# 	if format == "matrices":
+		# 		figure = self.create_flow_matrix(pd.Timestamp(index_str))
+		# 		figure_list.append(figure)
+
+		# 	elif format == "images":
+		# 		figure = self.plot_timestamp(pd.Timestamp(index_str))
+		# 		figure.update_coloraxes(showscale=False)
+		# 		figure.update_layout(width=res, height=res)
+		# 		buf = io.BytesIO(figure.to_image(format="png"))
+		# 		img = Image.open(buf)
+		# 		figure_array = np.asarray(img)
+		# 		figure_list.append(figure)
+
+		# 	bar.update()
 		
 		figure_array = np.expand_dims(np.array(figure_list), axis=-1)
 		np.save(figure_output, figure_array)
--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 available_map_zones = [
     "AT", "BE", "BG", "CH", "CZ", "DE_LU", "DK_1", "DK_2", "EE",
-    "ES", "FI", "FR", "GR", "HR", "HU", "IT_CNOR", 
+    "ES", "FI", "FR", "GR", "HR", "HU", "IT_CALA", "IT_CNOR", 
     "IT_CSUD", "IT_NORD", "IT_SARD", "IT_SICI", "IT_SUD", "LT", 
     "LV", "NL", "NO_1", "NO_2", "NO_3", "NO_4", "NO_5", "PL", "PT", 
     "RO", "RS", "SE_1", "SE_2", "SE_3", "SE_4", "SI", "SK"]
